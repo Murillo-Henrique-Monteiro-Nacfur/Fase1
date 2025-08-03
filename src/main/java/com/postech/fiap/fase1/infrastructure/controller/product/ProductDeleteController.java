5c0781526727cfebf1c61b342aa7204c8c815a97
Willian <williavizi@gmail.com>
2025-08-03 19:39:00 -0300

    Ajuste delete session da controller

--- src/main/java/com/postech/fiap/fase1/infrastructure/controller/product/ProductDeleteController.java
+++ src/main/java/com/postech/fiap/fase1/infrastructure/controller/product/ProductDeleteController.java
@@ -1,10 +1,10 @@
 package com.postech.fiap.fase1.infrastructure.controller.product;
 
 import com.postech.fiap.fase1.core.controllers.product.ProductDeleteCoreController;
-import com.postech.fiap.fase1.core.presenter.ProductPresenter;
 import com.postech.fiap.fase1.infrastructure.data.DataRepository;
 import com.postech.fiap.fase1.infrastructure.session.SessionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,14 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class ProductDeleteController implements ProductControllerInterface {
 
-    private final ProductPresenter productPresenter;
     private final DataRepository dataRepository;
     private final SessionRepository sessionRepository;
 
     @PreAuthorize("hasRole('CLIENT')")
     @DeleteMapping("/{id}")
-    public void createProduct(@PathVariable Long id) {
+    public ResponseEntity<Object> deleteProduct(@PathVariable Long id) {
         ProductDeleteCoreController productDeleteCoreController = new ProductDeleteCoreController(dataRepository, sessionRepository);
         productDeleteCoreController.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/postech/fiap/fase1/infrastructure/controller/product/ProductReadController.java
+++ src/main/java/com/postech/fiap/fase1/infrastructure/controller/product/ProductReadController.java
@@ -1,11 +1,9 @@
 package com.postech.fiap.fase1.infrastructure.controller.product;
 
-import com.postech.fiap.fase1.core.controllers.product.ProductCreateCoreController;
 import com.postech.fiap.fase1.core.controllers.product.ProductReadCoreController;
 import com.postech.fiap.fase1.core.dto.product.ProductDTO;
 import com.postech.fiap.fase1.core.presenter.ProductPresenter;
 import com.postech.fiap.fase1.infrastructure.data.DataRepository;
-import com.postech.fiap.fase1.infrastructure.session.SessionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,13 +27,13 @@ public class ProductReadController implements ProductControllerInterface {
     @GetMapping("/{id}")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
         ProductReadCoreController productReadCoreController = new ProductReadCoreController(dataRepository);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productPresenter.toDTO(productReadCoreController.getById(id)));
+        return ResponseEntity.status(HttpStatus.OK).body(productPresenter.toDTO(productReadCoreController.getById(id)));
     }
 
     @PreAuthorize("hasRole('CLIENT')")
     @GetMapping("/restaurant/{idRestaurant}")
     public ResponseEntity<List<ProductDTO>> findByIdRestaurant(@PathVariable Long idRestaurant) {
         ProductReadCoreController productReadCoreController = new ProductReadCoreController(dataRepository);
-        return ResponseEntity.status(HttpStatus.CREATED).body(productReadCoreController.getByIdRestaurant(idRestaurant).stream().map(productPresenter::toDTO).toList());
+        return ResponseEntity.status(HttpStatus.OK).body(productReadCoreController.getByIdRestaurant(idRestaurant).stream().map(productPresenter::toDTO).toList());
     }
 }
